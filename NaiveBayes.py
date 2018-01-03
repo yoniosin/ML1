a7dc1d0c538c8a9c554ef0155df2460e9569c5cd
yonio <yoni.osin@gmail.com>
2018-01-03 19:58:38 +0200

    NAiveBayes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,16 +1,15 @@
 import numpy as np
-import numpy.matlib
 import pylab as P
 
 
 def trainNaiveBayes(samples, classes_num, real_labels):
-    samples_ammount = len(samples)
+    samples_amount = len(samples)
     mat_list = []
     cluster_sizes = np.zeros(classes_num)
     for i in range(classes_num):
         mat_list.append(np.zeros((len(samples[0]), 0)))
 
-    for index in range(samples_ammount):
+    for index in range(samples_amount):
         curr_label = int(real_labels[index])
         mat_list[curr_label] = np.append(mat_list[curr_label], samples[index][:, None], axis=1)
         cluster_sizes[curr_label] += 1
@@ -23,8 +22,7 @@ def trainNaiveBayes(samples, classes_num, real_labels):
 def BinaryClassification(samples, cluster_sizes, mu, sigma):
     pdf = []
     for i in range(2):
-        test = np.transpose(P.normpdf(samples, mu[i], sigma[i]))
-        pdf.append(test)
+        pdf.append(np.transpose(P.normpdf(samples, mu[i], sigma[i])))
 
     our_classification = P.prod(pdf[0], axis=0) * cluster_sizes[0] < P.prod(pdf[1], axis=0) * cluster_sizes[1]
     return our_classification
